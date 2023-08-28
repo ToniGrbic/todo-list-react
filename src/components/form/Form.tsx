9b0263c3915508b101f17126671acc05a84a38e4
ToniGrbic <tonigrbic.5@gmail.com>
2023-08-28 22:42:33 +0200

    tsconfig setting changed

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -2,11 +2,15 @@ import React, { ReactElement, useMemo } from "react";
 import { useGlobalContext } from "../../state/context";
 import { TodoAppContext } from "../../types/todos";
 import { FormSelect, TextInput, DateTimeInput } from "../index";
-import { showArr, sortArr } from "../../utils/utils";
+//import { showArr, sortArr } from "../../utils/utils";
+
+const sortArr = ["Newest", "Oldest", "Date Ascending", "Date Descending"];
+
+const showArr = ["All", "Completed", "Uncompleted"];
 
 const Form = (): ReactElement => {
   const { handleSubmit, handleShowSelect, handleSortSelect } =
-          useGlobalContext() as TodoAppContext;
+    useGlobalContext() as TodoAppContext;
 
   return (
     <form id="Form" onSubmit={handleSubmit}>
