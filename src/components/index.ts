310cafcdfee735d915912f68f226c501392147b2
ToniGrbic <tonigrbic.5@gmail.com>
2023-08-28 22:47:46 +0200

    reverted ts setting

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 export { default as Alert } from "./Alert";
-export { default as Form } from "./Form/Form";
+export { default as Modal } from "./Modal";
+
 export { default as List } from "./List/List";
 export { default as Todo } from "./List/Todo";
 export { default as ClearButton } from "./List/ClearButton";
-export { default as Modal } from "./Modal";
-export {default as  FormSelect } from "./Form/FormSelect";
-export {default as  TextInput } from "./Form/TextInput";
-export {default as  DateTimeInput } from "./Form/DateTimeInput";
+
+export { default as Form } from "./Form/Form";
+export { default as FormSelect } from "./Form/FormSelect";
+export { default as TextInput } from "./Form/TextInput";
+export { default as DateTimeInput } from "./Form/DateTimeInput";
